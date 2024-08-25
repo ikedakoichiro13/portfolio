8c6343697463c1a831ba6fe047bd5ca0fe49f3b2
ikedakoichiro13 <git@github.com:ikedakoichiro13/Bookers.git>
2024-08-25 13:13:29 +0000

    apfp-3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,24 @@
 class PostsController < ApplicationController
-  
+
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to root_path
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :title, :kinds, :age, :gender, :health_condition, :vaccination, :infertility_treatmen, :features :transfer_condition)
 end
