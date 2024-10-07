e065107b2f2c7ad2ae6a21260b25d4dcd30fd18e
ikedakoichiro13 <git@github.com:ikedakoichiro13/Bookers.git>
2024-10-07 12:58:39 +0000

    apfp-end

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 Admin.create!(
-  email: "apfp_matching@gmail.com",
+  email: "apfp@gmail.com",
   password: "apfpmatching"
 )
 # This file should contain all the record creation needed to seed the database with its default values.
